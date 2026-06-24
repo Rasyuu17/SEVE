eed5de33ce8d49375df93a90e56310b6fe80eda3
Rasyuu17 <rasyuu.17@gmail.com>
2026-06-24 11:57:10 -0400

    Revert "Uploader + Repo"
    This reverts commit d82eb468bd3e8cb1e5561b9f56051c72e640f884.

--- uploader/src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    basePath: process.env.STORAGE_PATH || 'C:/repo',
-    limits: {
-        fileSize: 50 * 1024 * 1024,
-        files: 1
-    },
-    port: process.env.PORT || 3001,
-}
